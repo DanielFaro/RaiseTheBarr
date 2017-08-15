d4fb228acf6d1e7dc8bc7684340a5f11168464e8
Dan Faro <danieln.faro@gmail.com>
2017-08-15 14:34:42 -0400

    aug 15 update

--- assets/js/Module.js
+++ assets/js/Module.js
@@ -2,46 +2,35 @@ const Content = db.ref('/Content');
 const StartButton = document.getElementById('startmodule');
 const NextButton = document.getElementById('nextslide');
 const PreviousButton = document.getElementById('previousslide');
+const TakeQuizButton = document.getElementById('takequiz');
+const firstSlideHeading = document.getElementById('first-slide-heading');
+const firstSlideImg = document.getElementById('first-slide-img');
 const slideTxt = document.getElementById('slide-txt');
 const slideImg = document.getElementById('slide-img');
-var imgsrc = [];
-var modNum;
-
+const flexcontainer = document.getElementsByClassName('flex-container');
+const ul = document.getElementById("ul");
+var imgsrc = []; var training = [];
+var modNum; var SlideTot;
 let n = 0;
-var training = [];
+
 
 const getModuleIndex = (hash) => {
-    switch(hash) {
-        case '#1': return 1; 
-        case '#2': return 2;
-        case '#3': return 3;
-        case '#4': return 4;
-        case '#5': return 5;
-        case '#6': return 6;
-        case '#7': return 7;
-        case '#8': return 8;
-        case '#9': return 9;
-        case '#10': return 10;
+    switch (hash) {
+        case '#1': return 1; case '#2': return 2; case '#3': return 3; case '#4': return 4;case '#5': return 5;
+        case '#6': return 6; case '#7': return 7;case '#8': return 8; case '#9': return 9; case '#10': return 10;
         default: return 1;
     }
 }
 
+var ModFill = function () {  //fill training array with slides
 
+    Content.once('value', (snapshot) => {
 
-console.log("content.js loaded");
-
-
-
-
-var ModFill = function () {
-    //var modNum = getModuleIndex(window.location.hash);
-    //console.log(modNum);
-   // var training = [];
-    Content.on('value', (snapshot) => {  //fill Slide arrays with text and img sources from database
-        console.log('on.value');
-        for (var i = 1; i < 11; i++) {
+        SlideTot = snapshot.child("Module" + modNum + "/SlideTot").val();   //set SlideTot to number of slides contained in module 
+        
+        for (var i = 1; i <= SlideTot; i++) {
             let texts = [];
-            
+
             for (var x = 1; x <= 2; x++) {
                 let mains = snapshot.child("Module" + modNum + "/" + i + "/Text" + x + "/Main/").val();
                 texts.push(mains);
@@ -55,31 +44,22 @@ var ModFill = function () {
 
             const slide = {
                 img: snapshot.child("Module" + modNum + "/" + i + "/Img").val(),
-                title: snapshot.child("Module " + modNum + "/" + i + "/Title").val(),
-                heading: snapshot.child("Module " + modNum + "/" + i + "/Heading").val(),
+                title: snapshot.child("Module" + modNum + "/" + i + "/Title").val(),
+                heading: snapshot.child("Module" + modNum + "/" + i + "/Heading").val(),
                 text: texts
             }
-            //console.log(slide.text);
             training.push(slide);
-            console.log(training[i - 1].text);
         }// all slides added to training.
     })
-   
-};//end of module
+};//end of ModFill
 
-const callModFill = (function() {
+const callModFill = (function () {
+   slideImg.style.display = 'none';  //hide flex img element on first loading
+    slideTxt.style.display = 'none';  //hide flex text element on first loading
     modNum = getModuleIndex(window.location.hash);
     ModFill(modNum);
-
 })();
 
-
-
-//convert text array to unorderedlist
-
-//function test1() {
-//  console.log(slidecontent.training[1].text);
-//}
 function arrToUl(arr) {
     var ul = document.createElement('ul'), li;
     for (var i = 0; i < arr.length; i++) {
@@ -112,63 +92,65 @@ function toggleorder(element, element2) {
 /*-------------Previous and Next slide functions---------------*/
 
 function previousSlide() {
+    n--;
+    TakeQuizButton.style.visibility = "hidden"; //hide take quiz button
+    NextButton.style.visibility = 'visible'; //show Next Button
+    slideTxt.removeChild(slideTxt.firstChild);//remove any current text/children in the text box
 
-    if (n == 1 || n == 2) {   //can never be given n = 0 due to 'Previous' button being hidden for n=0;
-        n--;
+    if (n == 1) {   //can never be given n = 0 due to 'Previous' button being hidden for n=0;
+        slideImg.style.display = 'none';  //hide flex img element
+        slideTxt.style.display = 'none';  //hide flex text element
         PreviousButton.style.visibility = 'hidden';    //Set first slide in stone -> hide previous button, img first, text second
-        slideImg.style.order = '1';
-        slideTxt.style.order = '2';
-    } else if (n < 10) {
-        n--;
-        toggleorder(slideTxt, slideImg);
-    } else {
-        toggleorder(slideTxt, slideImg)
-        n--;
-        NextButton.style.visibility = 'visible';
-        NextButton.innerHTML = "Next";
-    }
+        firstSlideHeading.style.display = 'block';
+        firstSlideImg.style.display = 'block';
+        firstSlideHeading.innerHTML = training[n - 1].heading;
+        firstSlideImg.src = training[n - 1].img;
 
-    slideImg.src = training[n - 1].img;
-    slideTxt.appendChild(arrToUl(training[n - 1].text));
+    } else if (n < SlideTot) {
+        toggleorder(slideTxt, slideImg);
+      //  flexcontainer.style.display = 'flex';
+        slideImg.src = training[n - 1].img;
+        slideTxt.appendChild(arrToUl(training[n - 1].text));
+    } 
 }
 
 function nextSlide() {
-    //console.log(training[0].text);
-    //getTraining();
-   //StartButton.style.visibility = 'hidden';  //hide startmodule button
-    //NextButton.style.visibility = 'visible';  //show nextslide button
-    if (modNum == 1 && n == 0) {
-        PreviousButton.style.visibility = 'hidden';    //Set first slide in stone -> hide previous button, img first, text second. 
-        slideImg.style.order = '1';
-        slideTxt.style.order = '2';
-        slideTxt.appendChild(arrToUl(training[0].heading));
-        slideImg.src = (training[0].img);
-        n++
-    }
-    else if (n == 0) {
+    console.log(n);
+    //make it so it doesn't need to check the follwing every time
+  
+
+    if (n == 0) {
         console.log("first slide created");
+        StartButton.style.visibility = 'hidden';  //hide startmodule button
+        NextButton.style.visibility = 'visible';  //show nextslide button
         PreviousButton.style.visibility = 'hidden';    //Set first slide in stone -> hide previous button, img first, text second. 
-        slideImg.style.order = '1';
-        slideTxt.style.order = '2';
-        //slideTxt.innerHTML = slidecontent[0].text;
-        slideTxt.appendChild(arrToUl(training[0].text));
-        //arrToUl(slidecontent[0].text);
-        slideImg.src = (training[0].img);
-        //console.log(slideTxt.innerHTML);
-        //console.log(slideImg.src);
+        //firstSlideHeading.style.display = 'block';
+        //firstSlideImg.style.display = 'block';
+        firstSlideHeading.innerHTML = training[0].heading;
+        firstSlideImg.src = training[0].img;
+        //slideImg.toggleClass('hidden');  //hide flex img element
+        //slideTxt.toggleClass('hidden');  //hide flex text element
         n++;
     }
-    else if (n < 10) {
+    else if (n < SlideTot) {
         PreviousButton.style.visibility = 'visible'   //show 'Previous' button if n >=1;
+        firstSlideHeading.style.display = 'none';
+        firstSlideImg.style.display = 'none';
         toggleorder(slideTxt, slideImg);
+        slideImg.style.display = 'block';
+        slideTxt.style.display = 'block';
         slideImg.src = training[n].img;
-        slidTxt.appenChild(arrToUl(training[n].text));
-        //slideTxt.innerHTML = slidecontent[n].text;
+        if (slideTxt.hasChildNodes()) {
+            slideTxt.removeChild(slideTxt.firstChild);//remove any current text/children in the text box
+        }
+        slideTxt.appendChild(arrToUl(training[n].text)); //append new text
         n++;
-        if (n == 10) {
-            NextButton.innerHTML = "Take the Quiz";
+        if (n == SlideTot) {
+            NextButton.style.visibility = 'hidden';
+            TakeQuizButton.style.visibility = "visible";
         }
     }
+
 }
 
 
